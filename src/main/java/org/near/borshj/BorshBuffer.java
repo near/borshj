3d05acfb393d58c69262114d48843494e4477854
Arto Bendiken <arto@bendiken.net>
2020-10-05 11:49:02 +0300

    Implemented BorshBuffer#writeU*().

--- src/main/java/org/near/borshj/BorshBuffer.java
+++ src/main/java/org/near/borshj/BorshBuffer.java
@@ -7,6 +7,7 @@ import static java.util.Objects.requireNonNull;
 import androidx.annotation.NonNull;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.math.BigInteger;
 import java.util.Arrays;
 
 public class BorshBuffer {
@@ -28,9 +29,47 @@ public class BorshBuffer {
   public @NonNull byte[] toByteArray() {
     assert(this.buffer.hasArray());
     final int arrayOffset = this.buffer.arrayOffset();
-    return Arrays.copyOfRange(
-      this.buffer.array(),
-      arrayOffset + this.buffer.position(),
-      arrayOffset + this.buffer.limit());
+    return Arrays.copyOfRange(this.buffer.array(),
+      arrayOffset, arrayOffset + this.buffer.position());
+  }
+
+  public @NonNull BorshBuffer writeU8(final int value) {
+    return this.writeU8((byte)value);
+  }
+
+  public @NonNull BorshBuffer writeU8(final byte value) {
+    this.buffer.put(value);
+    return this;
+  }
+
+  public @NonNull BorshBuffer writeU32(final int value) {
+    this.buffer.putInt(value);
+    return this;
+  }
+
+  public @NonNull BorshBuffer writeU64(final long value) {
+    this.buffer.putLong(value);
+    return this;
+  }
+
+  public @NonNull BorshBuffer writeU128(final long value) {
+    return this.writeU128(BigInteger.valueOf(value));
+  }
+
+  public @NonNull BorshBuffer writeU128(final @NonNull BigInteger value) {
+    if (value.signum() == -1) {
+      throw new ArithmeticException("integer underflow");
+    }
+    if (value.bitLength() > 128) {
+      throw new ArithmeticException("integer overflow");
+    }
+    final byte[] bytes = value.toByteArray();
+    for (int i = bytes.length - 1; i >= 0; i--) {
+      this.buffer.put(bytes[i]);
+    }
+    for (int i = 0; i < 16 - bytes.length; i++) {
+      this.buffer.put((byte)0);
+    }
+    return this;
   }
 }
--- src/test/java/BorshBufferTests.java
+++ src/test/java/BorshBufferTests.java
@@ -13,4 +13,45 @@ public class BorshBufferTests {
   void newBuffer() {
     buffer = BorshBuffer.allocate(256);
   }
+
+  @Test
+  void writeU8() {
+    buffer.writeU8(0x42);
+    final byte[] expected = new byte[] {0x42};
+    final byte[] actual = buffer.toByteArray();
+    assertEquals(expected.length, actual.length);
+    assertArrayEquals(expected, actual);
+  }
+
+  @Test
+  void writeU32() {
+    buffer.writeU32(0x00112233);
+    final byte[] expected = new byte[] {0x33, 0x22, 0x11, 0x00};
+    final byte[] actual = buffer.toByteArray();
+    assertEquals(expected.length, actual.length);
+    assertArrayEquals(expected, actual);
+  }
+
+  @Test
+  void writeU64() {
+    buffer.writeU64(0x0011223344556677L);
+    final byte[] expected = new byte[] {
+      0x77, 0x66, 0x55, 0x44, 0x33, 0x22, 0x11, 0x00,
+    };
+    final byte[] actual = buffer.toByteArray();
+    assertEquals(expected.length, actual.length);
+    assertArrayEquals(expected, actual);
+  }
+
+  @Test
+  void writeU128() {
+    buffer.writeU128(0x0011223344556677L);
+    final byte[] expected = new byte[] {
+      0x77, 0x66, 0x55, 0x44, 0x33, 0x22, 0x11, 0x00,
+      0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    };
+    final byte[] actual = buffer.toByteArray();
+    assertEquals(expected.length, actual.length);
+    assertArrayEquals(expected, actual);
+  }
 }
