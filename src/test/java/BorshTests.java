38085b41ffe1f461293ef35714071a86ddca916d
Arto Bendiken <arto@bendiken.net>
2020-10-17 16:30:10 +0300

    Added recursive POJO round-trip test.

--- src/test/java/BorshTests.java
+++ src/test/java/BorshTests.java
@@ -7,36 +7,66 @@ import org.junit.jupiter.api.Test;
 import org.near.borshj.Borsh;
 
 public class BorshTests {
-  static public class Point2Di implements Borsh {
-    public int x;
-    public int y;
+  static public class Point2Df implements Borsh {
+    public float x;
+    public float y;
 
-    public Point2Di() {}
+    public Point2Df() {}
 
-    public Point2Di(final int x, final int y) {
+    public Point2Df(final float x, final float y) {
       this.x = x;
       this.y = y;
     }
 
     @Override
     public String toString() {
-      return String.format("Point2Di(%d, %d)", this.x, this.y);
+      return String.format("Point2Df(%f, %f)", this.x, this.y);
     }
 
     @Override
     public boolean equals(Object object) {
-      if (object == null || object.getClass() != this.getClass()) {
-        return false;
-      }
-      final Point2Di other = (Point2Di)object;
+      if (object == null || object.getClass() != this.getClass()) return false;
+      final Point2Df other = (Point2Df)object;
       return this.x == other.x && this.y == other.y;
     }
   }
 
+  static public class Rect2Df implements Borsh {
+    public Point2Df topLeft;
+    public Point2Df bottomRight;
+
+    public Rect2Df() {}
+
+    public Rect2Df(final Point2Df topLeft, final Point2Df bottomRight) {
+      this.topLeft = topLeft;
+      this.bottomRight = bottomRight;
+    }
+
+    @Override
+    public String toString() {
+      return String.format("Rect2Df(%s, %s)", this.topLeft.toString(), this.bottomRight.toString());
+    }
+
+    @Override
+    public boolean equals(Object object) {
+      if (object == null || object.getClass() != this.getClass()) return false;
+      final Rect2Df other = (Rect2Df)object;
+      return this.topLeft.equals(other.topLeft) && this.bottomRight.equals(other.bottomRight);
+    }
+  }
+
   @Test
-  void roundtripPoint2Di() {
-    final Point2Di pt = new Point2Di(123, 456);
-    assertEquals(pt, Borsh.deserialize(Borsh.serialize(pt), Point2Di.class));
+  void roundtripPoint2Df() {
+    final Point2Df point = new Point2Df(123, 456);
+    assertEquals(point, Borsh.deserialize(Borsh.serialize(point), Point2Df.class));
+  }
+
+  @Test
+  void roundtripRect2Df() {
+    final Point2Df topLeft = new Point2Df(-123, -456);
+    final Point2Df bottomRight = new Point2Df(123, 456);
+    final Rect2Df rect = new Rect2Df(topLeft, bottomRight);
+    assertEquals(rect, Borsh.deserialize(Borsh.serialize(rect), Rect2Df.class));
   }
 
   @Test
