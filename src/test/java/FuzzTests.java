10dcc22ec7078a435975535cb1343272c9a88d24
Arto Bendiken <arto@bendiken.net>
2020-10-19 08:51:23 +0300

    Implemented bean class generation for tests.

--- src/test/java/FuzzTests.java
+++ src/test/java/FuzzTests.java
@@ -3,20 +3,14 @@
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import net.sf.cglib.beans.BeanGenerator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.near.borshj.Borsh;
 
 public class FuzzTests {
-  private BeanGenerator generator;
-
-  @BeforeEach
-  void newGenerator() {
-    generator = new BeanGenerator();
-  }
-
-  static public class Base implements Borsh {
+  static public class Bean implements Borsh {
     @Override
     public String toString() {
       final StringBuilder buffer = new StringBuilder();
@@ -24,10 +18,16 @@ public class FuzzTests {
       buffer.append('(');
       try {
         for (final Field field : this.getClass().getDeclaredFields()) {
-          field.get(this); // TODO
+          field.setAccessible(true);
+          buffer.append(field.getName());
+          buffer.append('=');
+          buffer.append(field.get(this));
+          buffer.append(',');
         }
       }
-      catch (final IllegalAccessException e) {}
+      catch (final IllegalAccessException e) {
+        e.printStackTrace();
+      }
       buffer.append(')');
       return buffer.toString();
     }
@@ -45,13 +45,30 @@ public class FuzzTests {
         return true;
       }
       catch (final IllegalAccessException e) {
+        e.printStackTrace();
         return false;
       }
     }
   }
 
+  Object newPersonBean() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+    final BeanGenerator beanGenerator = new BeanGenerator();
+    beanGenerator.setSuperclass(Bean.class);
+    beanGenerator.addProperty("name", String.class);
+    beanGenerator.addProperty("email", String.class);
+    beanGenerator.addProperty("age", Integer.class);
+    beanGenerator.addProperty("twitter", Boolean.class);
+    final Object bean = beanGenerator.create();
+    bean.getClass().getMethod("setName", String.class).invoke(bean, "J. Random Hacker");
+    bean.getClass().getMethod("setEmail", String.class).invoke(bean, "jhacker@example.org");
+    bean.getClass().getMethod("setAge", Integer.class).invoke(bean, 42);
+    bean.getClass().getMethod("setTwitter", Boolean.class).invoke(bean, true);
+    return bean;
+  }
+
   @Test
-  void testGenerator() {
-    // TODO
+  void testBeanGenerator() throws Exception {
+    final Object bean = newPersonBean();
+    assertEquals(bean, Borsh.deserialize(Borsh.serialize(bean), bean.getClass()));
   }
 }
